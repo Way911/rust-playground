9ae31a9cf06a47f83472963830d2bc11a4887c3a
Way911 <wayne911@live.cn>
2022-04-10 13:59:33 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{
+    fs::File,
+    io::{Error, Read},
+};
+
+fn read_content_from_file(path: &str) -> Result<String, Error> {
+    let mut content: String = String::new();
+    File::open(path)?.read_to_string(&mut content)?;
+    Ok(content)
+}
+
 fn main() {
-    println!("Hello World!")
+    let path = "README.md";
+    let content = match read_content_from_file(path) {
+        Ok(content) => content,
+        Err(error) => panic!("Error: {}, Path: {}", error, path),
+    };
+    println!("path: {}, content: {}", path, content);
 }

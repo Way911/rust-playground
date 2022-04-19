16ceb425ac269153427f2148318ddb0a57a7c999
Way911 <wayne911@live.cn>
2022-04-19 13:18:54 +0800

    init - add search function

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+use std::{error::Error, fs};
+
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename.clone())?;
+
+    // println!("With text:\n{contents}");
+
+    let results = search(&config.query, &contents);
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search(query: &str, contents: &str) -> Vec<String> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .map(|line| line.to_string())
+        .collect()
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        return Ok(Config { query, filename });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
+use rust_playground::Config;
+
 fn main() {
-    println!("Hello World!")
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(args.as_slice()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    if let Err(s) = rust_playground::run(&config) {
+        eprintln!("Application error: {s} \n{config:#?}");
+        std::process::exit(1);
+    }
 }

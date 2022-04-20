be0b5efbac3196145a4ba43c0ce71158f2de9701
Way911 <wayne911@live.cn>
2022-04-20 19:10:16 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 fn main() {
-    println!("Hello World!")
+    let x = 5;
+    drop(x);
+    let y = MyBox::new((x, x));
+
+    assert_eq!(5, x);
+    assert_eq!((5, 5), *y);
 }

6ca61d117846c56638bd818bd61759db23b648d2
Way911 <wayne911@live.cn>
2022-04-21 15:27:59 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::rc::Rc;
+
+enum List {
+    Cons(isize, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
 fn main() {
-    println!("Hello World!")
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Rc::new(Cons(3, Rc::clone(&a)));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+
+    println!("count after c goes outof scope = {}", Rc::strong_count(&a));
 }

16148d029e45774038b42d3aa8b2fe2cadd4fc2d
Way911 <wayne911@live.cn>
2022-04-22 01:25:53 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use rust_playground::Post;
+
 fn main() {
-    println!("Hello World!")
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    println!("{}", post.get_content());
+
+    post.request_review();
+    println!("{}", post.get_content());
+
+    post.approve();
+    println!("{}", post.get_content());
 }

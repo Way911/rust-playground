92c29108b73c1f5285db5cd2f8d7a965d69197c5
Way911 <wayne911@live.cn>
2022-04-21 16:47:21 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 fn main() {
-    println!("Hello World!")
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m = {m:?}");
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let tmp_ct = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = tmp_ct.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
 }

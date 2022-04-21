16988d623271029649f260e541a73ad4790cb492
Way911 <wayne911@live.cn>
2022-04-21 16:02:53 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{sync::mpsc, thread, time::Duration};
+
 fn main() {
-    println!("Hello World!")
+    let (sender, receiver) = mpsc::channel();
+
+    let sender1 = mpsc::Sender::clone(&sender);
+    thread::spawn(move || {
+        let vals = vec!["hi", "from", "the", "thread", "hi", "from", "the", "thread"];
+        for val in vals {
+            sender1.send(format!("1:{val}")).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    let sender2 = mpsc::Sender::clone(&sender);
+    thread::spawn(move || {
+        let vals = vec!["hi", "from", "the", "thread", "hi", "from", "the", "thread"];
+        for val in vals {
+            sender2.send(format!("2:{val}")).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for val in receiver {
+        println!("{val}");
+    }
 }

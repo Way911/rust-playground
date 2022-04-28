03b10bbe0a2a7ead3265f5022be3631d1e71fdf9
Way911 <wayne911@live.cn>
2022-04-28 10:10:28 +0800

    add serde json

--- src/user.rs
+++ src/user.rs
@@ -14,8 +14,34 @@ impl User {
     pub fn new(name: String, age: u8, gender: Gender) -> Self {
         Self { name, age, gender }
     }
+}
 
-    pub fn persist(&self, filename: &str) -> Result<usize, io::Error> {
+pub trait JsonSerde<T> {
+    fn persist(&self, filename: &str) -> Result<usize, io::Error>;
+    fn load(filename: &str) -> Result<T, io::Error>;
+}
+
+impl JsonSerde<Self> for User {
+    fn persist(&self, filename: &str) -> std::result::Result<usize, std::io::Error> {
+        let json = serde_json::to_vec(self)?;
+        let mut file = File::create(filename)?;
+        file.write_all(&json)?;
+        Ok(json.len())
+    }
+    fn load(filename: &str) -> std::result::Result<Self, std::io::Error> {
+        let file = File::open(filename)?;
+        let user = serde_json::from_reader(file)?;
+        Ok(user)
+    }
+}
+
+pub trait BincodeSerde<T> {
+    fn persist(&self, filename: &str) -> Result<usize, io::Error>;
+    fn load(filename: &str) -> Result<T, io::Error>;
+}
+
+impl BincodeSerde<Self> for User {
+    fn persist(&self, filename: &str) -> Result<usize, io::Error> {
         let mut file = File::create(filename)?;
         // let data = match bincode::serialize_into(file, self) {
         //     Ok(it) => Ok(0),
@@ -26,7 +52,7 @@ impl User {
         Ok(data.len())
     }
 
-    pub fn load(filename: &str) -> Result<Self, io::Error> {
+    fn load(filename: &str) -> Result<Self, io::Error> {
         let file = File::open(filename)?;
         // let mut data = String::new();
         // file.read_to_string(&mut data)?;
@@ -60,13 +86,29 @@ impl Default for Gender {
 
 #[cfg(test)]
 mod user_tests {
-    use super::*;
+
+    use crate::user::User;
 
     #[test]
-    fn user_persist_and_load() {
-        let user1 = User::default();
-        user1.persist("./user1.bc").unwrap();
-        let user2 = User::load("./user1.bc").unwrap();
+    fn user_persist_and_load_bincode() {
+        use crate::user::BincodeSerde;
+
+        let mut user1 = User::default();
+        user1.name = "Tommy".into();
+        user1.persist("./tmp/user1.bc").unwrap();
+        let user2 = User::load("./tmp/user1.bc").unwrap();
+        assert_eq!(user1, user2);
+        // println!("{user1:?}");
+    }
+
+    #[test]
+    fn user_persist_and_load_json() {
+        use crate::user::JsonSerde;
+
+        let mut user1 = User::default();
+        user1.name = "Tommy".into();
+        user1.persist("./tmp/user1.json").unwrap();
+        let user2 = User::load("./tmp/user1.json").unwrap();
         assert_eq!(user1, user2);
         // println!("{user1:?}");
     }
